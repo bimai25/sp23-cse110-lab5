b6a9c4eb6881b65205035b37309226e949d0545f
bimai25 <bimai@ucsd.edu>
2023-05-03 22:10:23 -0700

    updated explore.js and expose.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,65 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const speech = window.speechSynthesis; //create the text to speech object
+  let voices; //this is an array of Voices
+  let voiceSelect = document.getElementById("voice-select"); //dropdown menu of voices
+  let talkBtn = document.querySelector("button"); //btn to activate speech
+
+  window.addEventListener('load', function(){
+    //load all the voices
+    voices = speech.getVoices();
+
+    //populate voices in drop down list
+    populateVoiceList();
+
+    console.log("voices loaded and ready in drop down");
+  })
+
+  talkBtn.addEventListener('click', function(){
+    console.log("talk btn pressed");
+    //get text from textbox
+    let msg = document.getElementById("text-to-speak").value;
+
+    //create a new object to speak the text
+    const toSpeak = new SpeechSynthesisUtterance(msg);
+
+    //get the voice from the dropdown
+    const voiceOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+
+    //set the voice to the selected option
+    for(let i = 0; i < voices.length; i++){
+      if(voices[i].name === voiceOption){
+        toSpeak.voice = voices[i];
+      }
+    }
+    //speak
+    speech.speak(toSpeak);
+    document.querySelector("img").src = "assets/images/smiling-open.png";
+    console.log("we are speaking");
+
+    //event listener for when utterance is done speaking
+    toSpeak.addEventListener('end', function(){
+      console.log("we are done speaking");
+      document.querySelector("img").src = "assets/images/smiling.png";
+    })
+  })
+
+  function populateVoiceList() {
+    voices = speech.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,74 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  let hornOptions = document.getElementById("horn-select");
+  let volumeControl = document.getElementById("volume");
+  let playAudioBtn = document.querySelector("button");
+  const jsConfetti = new JSConfetti(); //create a confetti object
+  let shootConfetti = false;
+
+  hornOptions.addEventListener('change', function(){
+    //check the value of the horn option element and set the image and
+    //audio accordingly
+    if(hornOptions.value == "air-horn"){
+      console.log("air horn selected");
+      let hornImg = document.querySelector("[alt='No image selected']");
+      let hornAudio = document.getElementsByClassName("hidden");
+      hornAudio[0].src = "assets/audio/air-horn.mp3";
+      hornImg.src = "assets/images/air-horn.svg";
+      shootConfetti = false;
+    }
+    if(hornOptions.value == "car-horn"){
+      console.log("car horn selected");
+      let hornImg = document.querySelector("[alt='No image selected']");
+      let hornAudio = document.getElementsByClassName("hidden");
+      hornAudio[0].src = "assets/audio/car-horn.mp3";
+      hornImg.src = "assets/images/car-horn.svg";
+      shootConfetti = false;
+    }
+    if(hornOptions.value == "party-horn"){
+      console.log("party horn selected");
+      let hornImg = document.querySelector("[alt='No image selected']");
+      let hornAudio = document.getElementsByClassName("hidden");
+      hornAudio[0].src = "assets/audio/party-horn.mp3";
+      hornImg.src = "assets/images/party-horn.svg";
+      shootConfetti = true;
+    }
+  })
+
+  volumeControl.addEventListener('change', function(){
+    //check the volume level and set the picture accordingly
+    console.log(volumeControl.value);
+
+    //set the volume of the audio element
+    document.getElementsByClassName("hidden").volume = volumeControl.value * 0.01;
+    if(volumeControl.value == 0){
+      console.log("volume control 0");
+      let img = document.querySelector("[alt='Volume level 2']");
+      img.src = "assets/icons/volume-level-0.svg";
+    }
+    if(volumeControl.value >= 1 && volumeControl.value < 33){
+      console.log("volume control 1");
+      let img = document.querySelector("[alt='Volume level 2']");
+      img.src = "assets/icons/volume-level-1.svg";
+    }
+    if(volumeControl.value >= 33 && volumeControl.value < 67){
+      console.log("volume control 2");
+      let img = document.querySelector("[alt='Volume level 2']");
+      img.src = "assets/icons/volume-level-2.svg";
+    }
+    if(volumeControl.value >= 67){
+      console.log("volume control 3");
+      let img = document.querySelector("[alt='Volume level 2']");
+      img.src = "assets/icons/volume-level-3.svg";
+    }
+  })
+
+  playAudioBtn.addEventListener('click', function(){
+    document.querySelector("audio").play();
+
+    if(shootConfetti == true){
+      jsConfetti.addConfetti();
+    }
+  })
 }
